804f6a9351836d292566fbec3322816241143fe2
Mohammed Al-Dahleh <maldahleh@gmail.com>
2019-11-01 20:16:52 -0400

    Add some validation

--- request/data_request.go
+++ request/data_request.go
@@ -1,11 +1,65 @@
 package request
 
+import "strings"
+
+var provinces = [...]string{
+	"AB",
+	"BC",
+	"MB",
+	"NB",
+	"NL",
+	"NS",
+	"NT",
+	"NU",
+	"ON",
+	"PE",
+	"QC",
+	"SK",
+	"YT",
+}
+
 type StationRequest struct {
 	Province *string        `json:"province"`
 	Months   *map[int][]int `json:"months"`
 }
 
+func (r *StationRequest) validateProvince() bool {
+	*r.Province = strings.ToUpper(*r.Province)
+	for _, value := range provinces {
+		if value == *r.Province {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (r *StationRequest) validateMonths() bool {
+	for key, value := range *r.Months {
+		if key < 1900 || key > 2019 {
+			return false
+		}
+
+		for _, month := range value {
+			if month < 1 || month > 12 {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func (r *StationRequest) validate() bool {
+	return r.Province != nil && r.Months != nil && r.validateProvince() && r.validateMonths()
+}
+
 type DataRequest struct {
 	Data       *map[string]StationRequest `json:"data"`
 	DataPoints *[]string                  `json:"points"`
+}
+
+func (* DataRequest) Validate() bool {
+	// TODO
+	return true
 }
