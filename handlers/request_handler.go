756c36f999ca5e20d3ad299ddbad7a3ae1c9f022
Mohammed Al-Dahleh <maldahleh@gmail.com>
2019-11-10 17:02:59 -0500

    Use goroutines to improve API response times

--- handlers/request_handler.go
+++ handlers/request_handler.go
@@ -19,86 +19,126 @@ type monthlyData map[string]dailyData
 type dailyData map[string]pointData
 type pointData map[string]string
 
+type dataChannel struct {
+	year  string
+	month string
+	data  dailyData
+}
+
 func RetrieveData(stationId string, station request.StationRequest, dataPoints *[]string) YearlyData {
 	data := make(YearlyData)
+	channel := make(chan dataChannel)
+
+	channelCalls := 0
 	for yearRaw, months := range *station.Months {
 		year := strconv.Itoa(yearRaw)
 
 		monthData := make(monthlyData)
 		for _, monthRaw := range months {
 			month := stringifyMonth(monthRaw)
-			path := stationId + "-" + year + "-" + month + ".csv"
+			channelCalls++
 
-			err := utils.DownloadFile(path, createUrl(stationId, *station.Province, year, month))
-			if err != nil {
-				log.Error("failed to download file", path, "error", err)
-				continue
-			}
-
-			file, err := os.Open(path)
-			if err != nil {
-				log.Error("failed to open file", path, "error", err)
-				utils.DeleteFile(path)
-				continue
-			}
-
-			scanner := bufio.NewScanner(file)
-			dayData := make(dailyData)
-			headerLine := 0
-			for scanner.Scan() {
-				if headerLine < 25 {
-					headerLine++
-					continue
-				}
-
-				text := scanner.Text()
-				if text == "" {
-					continue
-				}
-
-				dayPointData := make(pointData)
-				record := strings.Split(text, ",")
-				for _, requestedPoint := range *dataPoints {
-					switch requestedPoint {
-					case "maxtemp":
-						dayPointData[requestedPoint] = strings.Trim(record[5], "\"")
-					case "mintemp":
-						dayPointData[requestedPoint] = strings.Trim(record[7], "\"")
-					case "meantemp":
-						dayPointData[requestedPoint] = strings.Trim(record[9], "\"")
-					case "rain":
-						dayPointData[requestedPoint] = strings.Trim(record[15], "\"")
-					case "snow":
-						dayPointData[requestedPoint] = strings.Trim(record[17], "\"")
-					case "precip":
-						dayPointData[requestedPoint] = strings.Trim(record[19], "\"")
-					case "snowgrnd":
-						dayPointData[requestedPoint] = strings.Trim(record[21], "\"")
-					case "maxgust":
-						dayPointData[requestedPoint] = strings.Trim(record[25], "\"")
-					}
-				}
-
-				dayData[strings.Trim(record[3], "\"")] = dayPointData
-			}
-
-			if err := scanner.Err(); err != nil {
-				log.Fatal("error reading", file, "error", err)
-			}
-
-			err = file.Close()
-			if err != nil {
-				log.Error("failed to close file", path, "error", err)
-			}
-
-			utils.DeleteFile(path)
-			monthData[month] = dayData
+			go downloadData(stationId, *station.Province, year, month, dataPoints, channel)
 		}
 
 		data[year] = monthData
 	}
 
+	for i := 0; i < channelCalls; i++ {
+		channelData := <-channel
+		if channelData.data == nil {
+			continue
+		}
+
+		data[channelData.year][channelData.month] = channelData.data
+	}
+
 	return data
+}
+
+func downloadData(stationId string, province string, year string, month string, points *[] string, channel chan dataChannel) {
+	path := stationId + "-" + year + "-" + month + ".csv"
+
+	err := utils.DownloadFile(path, createUrl(stationId, province, year, month))
+	if err != nil {
+		log.Error("failed to download file", path, "error", err)
+		channel <- dataChannel{
+			year:  "",
+			month: "",
+			data:  nil,
+		}
+
+		return
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Error("failed to open file", path, "error", err)
+		utils.DeleteFile(path)
+		channel <- dataChannel{
+			year:  "",
+			month: "",
+			data:  nil,
+		}
+
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+	dayData := make(dailyData)
+	headerLine := 0
+	for scanner.Scan() {
+		if headerLine < 25 {
+			headerLine++
+			continue
+		}
+
+		text := scanner.Text()
+		if text == "" {
+			continue
+		}
+
+		dayPointData := make(pointData)
+		record := strings.Split(text, ",")
+		for _, requestedPoint := range *points {
+			switch requestedPoint {
+			case "maxtemp":
+				dayPointData[requestedPoint] = strings.Trim(record[5], "\"")
+			case "mintemp":
+				dayPointData[requestedPoint] = strings.Trim(record[7], "\"")
+			case "meantemp":
+				dayPointData[requestedPoint] = strings.Trim(record[9], "\"")
+			case "rain":
+				dayPointData[requestedPoint] = strings.Trim(record[15], "\"")
+			case "snow":
+				dayPointData[requestedPoint] = strings.Trim(record[17], "\"")
+			case "precip":
+				dayPointData[requestedPoint] = strings.Trim(record[19], "\"")
+			case "snowgrnd":
+				dayPointData[requestedPoint] = strings.Trim(record[21], "\"")
+			case "maxgust":
+				dayPointData[requestedPoint] = strings.Trim(record[25], "\"")
+			}
+		}
+
+		dayData[strings.Trim(record[3], "\"")] = dayPointData
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("error reading", file, "error", err)
+	}
+
+	err = file.Close()
+	if err != nil {
+		log.Error("failed to close file", path, "error", err)
+	}
+
+	utils.DeleteFile(path)
+	channel <- dataChannel{
+		year:  year,
+		month: month,
+		data:  dayData,
+	}
 }
 
 func createUrl(stationId string, province string, year string, month string) string {
